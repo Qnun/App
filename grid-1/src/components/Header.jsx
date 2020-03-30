4e856c24b88481aa4634942144098a413637b67d
unknown <kukkurruza@gmail.com>
2020-03-30 21:51:12 +0300

    13

---
+++ grid-1/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+    return (
+        <header className='header'>
+            <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1024px-React-icon.svg.png' >
+            </img>
+        </header>
+    );
+}
+
+export default Header;
---
+++ grid-1/src/components/Profile.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './Profile.css';
+
+function Profile() {
+    return (
+        <div className='content'>
+      <div>
+        <img src='https://million-wallpapers.ru/wallpapers/6/2/465959790129926/biryuzovyj-ozero.jpg'></img>
+      </div>
+      <div>
+        <img src='http://s1.1zoom.net/big0/545/Rodents_Hamsters_535181_1280x852.jpg'></img>
+      </div>
+      <div>
+        My post
+          <div>
+          New post
+          </div>
+        <div>
+          <div>
+            Post 1
+            </div>
+          <div>
+            Post 2
+            </div>
+        </div>
+      </div>
+    </div>
+    );
+}
+
+export default Profile;
